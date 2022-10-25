dc68b41f8b1a0c6ef007ec15082e52957c9c256e
d-create <55021212+d-create@users.noreply.github.com>
2022-10-25 22:31:05 +0530

    Add files via upload

---
+++ PingPong/src/main/java/com/mycompany/pingpong/GameFrame.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pingpong;
+
+/**
+ *
+ * @author HP1
+ */
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame(){
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong Game");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
---
+++ PingPong/src/main/java/com/mycompany/pingpong/PingPong.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.pingpong;
+
+/**
+ *
+ * @author HP1
+ */
+public class PingPong {
+
+    public static void main(String[] args) {
+        GameFrame frame = new GameFrame();
+    }
+}
